64ae367fbd89935e80af6eae76abd8a620905232
rachel wolf <55379275+rachelwol@users.noreply.github.com>
2025-02-12 01:34:58 +0200

    Time: 4 ms (93.72%), Space: 53.3 MB (98.03%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -3,16 +3,19 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
-    if(numRows == 1 || numRows >= s.length)
+var convert = function (s, numRows) {
+    if (numRows == 1 || numRows >= s.length)
         return s;
     let curr = 0;
     const rows = Array(numRows).fill('');
-    let next = -1;
-    for(const c of s){
-        rows[curr]+=c;
-        if(curr === 0 || curr === numRows -1)
-            next *= -1;
+    let next = 1;
+    for (const c of s) {
+        rows[curr] += c;
+        if (curr === 0)
+            next = 1;
+        else
+            if (curr === numRows - 1)
+                next = -1;
         curr += next;
     }
     return rows.join("");
