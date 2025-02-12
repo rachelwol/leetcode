0ba9cfba87b402bd8dd1103c6caa6daa36a29223
rachel wolf <55379275+rachelwol@users.noreply.github.com>
2025-02-12 12:30:30 +0200

    Time: 4 ms (96.49%), Space: 57.6 MB (90.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -6,14 +6,11 @@ var isPalindrome = function(x) {
     if(x < 0){
         return false;
     }
-    if(Math.floor(x/10) === 0){
-        return true;
-    }
+    
     const originNum = x;
     let reversedNum = 0;
     while(x > 0){
-        let lastNum = x%10;
-        reversedNum = reversedNum *10 + lastNum;
+        reversedNum = reversedNum *10 + x % 10;
         x = Math.floor(x/10);
     }
     return originNum === reversedNum;
