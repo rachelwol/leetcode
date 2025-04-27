c731dda80d0aae37d48cfc8101ed9d1d2f8cacba
rachel wolf <55379275+rachelwol@users.noreply.github.com>
2025-04-28 02:41:45 +0300

    Time: 2 ms (41.33%), Space: 58 MB (22.67%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.js
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.js
@@ -7,7 +7,7 @@ var countSubarrays = function(nums) {
     for(let i=0;i<nums.length-2;i++){
         let half = nums[i+1]/2;
         if(nums[i]+nums[i+2] == half){
-            sum++
+            sum++;
         }
     }
     return sum;
